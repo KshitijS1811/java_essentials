2ffa5a293d13cb1571ca813f39f6691e140e265e
KshitijS1811 <kshitijsatpute8@gmail.com>
2023-11-20 13:12:41 +0530

    day-2

---
+++ Swap.java
@@ -0,0 +1,20 @@
+public class Swap {
+
+    public static void main(String[] args) {
+        
+    int n1=10;
+    int n2=20;
+
+    int vada=n1;
+
+    
+    n1=n2;
+    n2=vada;
+    System.out.println(n1);
+    System.out.println(n2);
+    }
+    
+
+    
+    
+}
